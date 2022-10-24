44535cce4110b74aa10632141e221edb4f27223b
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-24 10:54:48 -0700

    python changes

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str.split(' ')[5] + " " + str.split(' ')[6] + " " + str.split(' ')[-4] + " " + str.split(' ')[0])
+print(str)
