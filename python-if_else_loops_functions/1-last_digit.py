ecc7b2c03366b1b56f0297f40b137275c5d75ca7
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-25 08:14:02 -0700

    function changes

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,4 +6,4 @@ if (int(repr(number)[-1])) > 5:
 elif (int(repr(number)[-1])) == 0:
     print(f"Last digit of {number} is {int(repr(number)[-1])} and is 0")
 elif (int(repr(number)[-1])) < 6 and (int(repr(number)[-1])) != 0:
-    print(f"Last digit of {number} is {int(repr(number)[-1])} and is less than 6 and not 0")
+    print(f"Last digit of {number} is -{int(repr(number)[-1])} and is less than 6 and not 0")
