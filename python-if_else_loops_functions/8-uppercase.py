c050c4928c9230aadc03461eeeed5366c1aea727
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-26 13:28:19 -0700

    function changes

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for y in str:
         if 123 > ord(y) > 96:
             y = chr(ord(y) - 32)
-            print('{}'.format9\y), end="")
+            print('{}'.format(y), end="")
             print('')
