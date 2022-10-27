e3cf9ee596294e01b9b54a2430dd9918ec072c7e
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-27 13:34:02 -0700

    function changes

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(int(str(number)[-1]))
+    if number < 0:
+        number = 0 - number
+    print("{}".format(number % 10), end='')
+    return(number % 10)
