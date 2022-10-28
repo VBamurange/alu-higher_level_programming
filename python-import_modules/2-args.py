e933ce84a65392279a7e020a49f16b5aea5b7e09
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-28 11:59:18 -0700

    import changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__ == "__main__":
     import sys
     if (len(sys.argv) - 1) != 1:
         print("{} arguments".format(len(sys.argv) - 1), end="")
@@ -13,4 +13,3 @@ if __name__=="__main__":
         if(i == 0):
             continue
         print("{}: {}".format((i), arg))
-
