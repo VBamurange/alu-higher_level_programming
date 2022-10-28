c96edb2ccf86e5479b9c1e981f15572c946aade6
Vicky Bamurange <v.bamurange@alustudent.com>
2022-10-28 10:54:42 -0700

    import changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
-if __name__ == "__main__":
-    nbr_of_args = len(argv) - 1
-    if nbr_of_args > 0:
-        if nbr_of_args == 1:
-            print("{} argument:".format(nbr_of_args))
-        else:
-            print("{} arguments:".format(nbr_of_args))
-            values = 1
-        for arg in argv[1:]:
-            print("{}: {}".format(values, arg))
-            values += 1
-        else:
-            print("{} arguments.".format(nbr_of_args))
+import sys
+args = len(sys.argv)
+if(args <= 1):
+    print("{} arguments.".format(args - 1))
+elif(args == 2):
+    print("{} argument:".format(args - 1))
+else:
+    print("{} arguments:".format(args - 1))
+for x in range(1,args):
+    print("{}: {}".format(x, args[x]))
