57145a4a5e5e68a00fa415089a28828dc9e5aa82
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-02 11:26:13 -0700

    structure changes

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        _tuple = (0, None)
-    else:
-        _tuple = (len(sentence), sentence[0])
+        sentence[0] = None
     print("Length: {:d} - First character: {}".format(len(sentence), sentence[0]))
