1b94933d296be0c34aba9bd9ac6a9b78cba4b6f9
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-04 14:37:11 -0700

    more changes

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -24,6 +24,6 @@ def roman_to_int(roman_string):
                     list_num = [rom_n.get(ch)]
                 else:
                     list_num.append(rom_n.get(ch))
-                    last_rom = rom_n.get(ch)
+                last_rom = rom_n.get(ch)
     num += to_subtract(list_num)
     return (num)
