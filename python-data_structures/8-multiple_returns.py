f6b35d9ca88c284e93108812b62c98b00c8c6e28
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-04 12:55:45 -0700

    atructure changes

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     sentence_len = len(sentence)
     if sentence_len == 0:
-        new_tuple = (sentece_len, None)
+        new_tuple = (sentence_len, None)
     else:
         new_tuple = (sentence_len, sentence[0])
     return new_tuple
