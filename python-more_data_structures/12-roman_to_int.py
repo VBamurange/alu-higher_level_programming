7483dbf45353387f1f0d7983586cac830b2c4374
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-04 14:40:52 -0700

    more changes

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -11,7 +11,7 @@ def roman_to_int(roman_string):
         return 0
     if not isinstance(roman_string, str):
         return 0
-    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C':100, 'D': 500, 'M': 1000}
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_n.keys())
     num = 0
     last_rom = 0
