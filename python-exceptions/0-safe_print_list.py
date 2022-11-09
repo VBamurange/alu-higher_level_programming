0fe4b6621ef4d80da8c17fbc30bedcd01c77088c
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-09 08:29:09 -0800

    exception changes

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,5 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end='')
             elements += 1
     except:
-        print()
-        return elements
+        pass
+    print() 
+    return elements
