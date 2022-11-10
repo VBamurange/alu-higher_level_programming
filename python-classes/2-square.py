f2e680452795c7efec0fc0b3387e3708a05f1a09
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-10 10:28:04 -0800

    class changes

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""size validation"""
+
+
+class Square:
+    """we are going to initialize the size attribute"""
+    def __init__(self, size=0):
+        """attribute size is equating to 0"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
