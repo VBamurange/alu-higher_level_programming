bcda17929a20beec7eafb13a3c20bd025ed0d93c
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-10 12:48:51 -0800

    class changes

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -4,45 +4,31 @@
 
 class Square:
     """we are going to initialize the size attribute"""
-
     def __init__(self, size=0):
-
         """attribute size is equating to 0"""
-
         self.size = size
 
     @property
-
     def size(self):
-
         """get size of the square"""
-
         return (self.__size)
 
     @size.setter
-
     def size(self, value):
-
         if not isinstance(value, int):
-
             raise TypeError("size must be an integer")
-
         elif value < 0:
-
-            raise ValueError("size must be >= 0")
-
+            raise ValueError("size must be >=0"
         self.__size = value
 
     def area(self):
-
         """to return the area of the square"""
-
         return(self.__size * self.__size)
 
     def my_print(self):
-        """printing a square"""
-        for i in range(0, self.__size)
-        [print("#", end='') for j in range(self.__size)]
-        print("")
-        if self.__size == 0:
-            print('')
+    """printing a square"""
+       for i in range(0, self.__size):
+           [print("#", end='') for j in range(self.__size)]
+           print ('')
+       if self.__size == 0:
+          print('')
