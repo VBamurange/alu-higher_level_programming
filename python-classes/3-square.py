2f88e6f8943fd91dd4658fcaa636b5503c155e7e
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-10 10:40:58 -0800

    class changes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -18,6 +18,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
     def area(self):
         """to return the area of the square"""
         return(self.__size * self.__size)
