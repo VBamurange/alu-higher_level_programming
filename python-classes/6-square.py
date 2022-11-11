31baf455f08f8a53c71b6a3a93b4d4574a305dbf
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-11 01:13:05 -0800

    class changes

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,7 +47,6 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
