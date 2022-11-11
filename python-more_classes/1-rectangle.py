47e94d947b2445604754f683c25462b0319a4601
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-11 02:03:58 -0800

    mclass changes

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""real definition of a rectagle"""
+
+
+class Rectangle:
+    """we are going to initiaize new variables"""
+    def __init__(self, width=0, height=0):
+        """initialized width and height"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """retrieve width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieve length"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
