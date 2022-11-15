56a71775cbd6db59f6a579efcd3887e014542d6f
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-15 10:56:23 -0800

    inheritance changes

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,12 +2,9 @@
 """my list"""
 
 
-class list:
+class MyList(list):
     """parent class"""
 
     def print_sorted(self):
         """sorted in ascending order"""
         return (sorted(self))
-class MyList(list):
-    """child class"""
-    pass
