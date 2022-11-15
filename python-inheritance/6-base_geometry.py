980361aba4d2b8b587c04b26c4983092cea25c45
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-15 11:50:23 -0800

    inheritance changhes

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""improve geometry"""
+
+
+class BaseGeometry:
+    """a new class"""
+    
+    def area(self):
+        """a new funtcion"""
+        raise Exception("area() is not implemented")
