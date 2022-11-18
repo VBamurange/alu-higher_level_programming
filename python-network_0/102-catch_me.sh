9f673647507fccf484f83e4e55317f4046941200
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-18 06:18:26 -0800

    network changes

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #make a request that causes the server to respond
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: HoberltonSchool" -d "user_id=98"
+curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin: HolbertonSchool" -d "user_id=98"
