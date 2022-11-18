c0307e98b0554792121d207a002c7766fe1090ae
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-18 04:57:18 -0800

    Task

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""student to json with filter"""
+"""Class to JSON."""
 
 
 class Student:
-    """class student"""
+    """Class to Json."""
 
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
@@ -11,15 +11,15 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """json class"""
+        """class to json"""
 
         if attrs is None or type(attrs) != list:
             return self.__dict__
         else:
-            x = {}
-            for y in attrs:
-                if type(y) != str:
+            temp = {}
+            for elem in attrs:
+                if type(elem) != str:
                     return self.__dict__
-                if y in self.__dict__.keys():
-                    x[y] = self.__dict__[elem]
-            return x
+                if elem in self.__dict__.keys():
+                    temp[elem] = self.__dict__[elem]
+            return temp
