250a72ae5570f47d50dc82947ed811d38f30e364
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-23 12:14:53 -0800

    network changes

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""time for an interview"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        url = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[2],sys.argv[1])
+        x = requests.get(url)
+        y = x.json()
+        for i in range(10):
+            print("{}: {}".format(x[i]["sha"],x[i]["commit"]["author"]["name"]))
+    except IndexError:
+        pass
