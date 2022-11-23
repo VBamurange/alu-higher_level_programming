3b9902a49fa7a541096d9201f2d1c0504756f0e0
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-23 10:14:17 -0800

    network changes

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+"""what's my status"""
+
+
+import urllib.request
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as link:
+        see = link.read()
+        print("Body response:")
+        print("\t- type: ()".format(type(see)))
+        print("\t- content: ()".format(see))
+        print("\t- utf8 content: ()".format(see.decode("utf-8")))
