4fc51da3344693e39276c84efd6c7af497c18b9b
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-23 11:37:11 -0800

    network changes

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -9,13 +9,13 @@ from sys import argv
 if __name__ == "__main__":
     q = argv[1] if len(argv) == 2 else ""
     x = "http://0.0.0.0:5000/search_user"
-    y = requests.post(x, data={'q' : q})
+    y = requests.post(x, data={'q': q})
     try:
         y_dict = y.json()
         id, name = y_dict.get('id'), y_dict.get("name")
         if len(y_dict) == 0 or not id or not name:
             print('No result')
         else:
-            print("[{}] {]".format(y_dict.get('id'), y_dict.get('name')))
+            print("[{}] {}".format(y_dict.get('id'), y_dict.get('name')))
     except:
         print("Not a valid JSON")
