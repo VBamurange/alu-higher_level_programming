eefdec1957bcc6424067bafe885d89dbf1666ab0
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-23 11:12:02 -0800

    network changes

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -8,5 +8,5 @@ import sys
 
 if __name__ == "__main__":
     x = {"email": sys.argv[2]}
-    y = requests.post(sys.argv[1], z=x)
+    y = requests.post(sys.argv[1], data=x)
     print("{}".format(y.text))
