8fb6340b1a706135e5d51062360ba748057cfb41
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-23 11:56:41 -0800

    network changes

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,11 +8,10 @@ import sys
 
 
 if __name__ == "__main__":
-
-x = 'https://api.github.com/user'
-user = sys.argv[1]
-y = sys.argv[2]
-z = HTTPBasicAuth(username=user, password=y)
-w = requests.get(x, v=z)
-u = w.json()
-print(u.get('id'))
+    x = 'https://api.github.com/user'
+    user = sys.argv[1]
+    y = sys.argv[2]
+    z = HTTPBasicAuth(username=user, password=y)
+    w = requests.get(x, v=z)
+    u = w.json()
+    print(u.get('id'))
