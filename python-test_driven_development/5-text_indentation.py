492885eecbba8a8544363e24bfb35a5ac27820a5
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-30 09:05:48 -0800

    tests changes

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""text identation"""
+"""Definea text-identation function."""
 
 
 def text_indentation(text):
-    """function for the identation"""
+    """Print text with two new lines after each '.', '?' and ':'."""
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    i = 0
-    
-    while i < len(text) and text[i] == ' ':
-        i += 1
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
-    while i < len(text):
-        print(text[i], end="")
-        if text[i] == "\n" or text[i] in ".?:":
-            if text[i] in ".?:":
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
                 print("\n")
-        i += 1
-            while i < len(text) and text[i] == ' ':
-                i += 1
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
             continue
-        i += 1
+        c += 1
