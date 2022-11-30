de575adb3d2d84e2a56a9eca38e123a162d90f77
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-30 08:48:48 -0800

    tests changes

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""text indentation"""
+
+
+def text_indentation(text):
+    """function for text"""
+    if text is not str:
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text) and text[i] == ' ':
+        i += 1
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "\n" or text[i] in ".?:":
+            if text[i] in ".?:":
+                print("\n")
+        i += 1
+        while i < len(text) and text[i] == ' ':
+            i += 1
+        continue
+    i += 1
+
