2fd7f97b6eee5cc565ce3aa14fb8b6937abc101c
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-30 08:38:12 -0800

    tests changes

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""print square"""
+
+
+def print_square(size):
+    """function for printing square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size > 0:
+        print(("#" * size + "\n") * size, end="")
