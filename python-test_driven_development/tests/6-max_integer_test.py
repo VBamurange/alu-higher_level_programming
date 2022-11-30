14dc2789f3260c620abd0d8410e5d1c9b1a7ea22
Vicky Bamurange <v.bamurange@alustudent.com>
2022-11-30 09:29:33 -0800

    tests changes

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """max integer-unittest"""
 
-
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 class MaxInt(unittest.TestCase):
@@ -9,13 +8,13 @@ class MaxInt(unittest.TestCase):
         self.assertEqual(max_integer([5, 6, 7, 8, 9]), 9)
         self.assertEqual(max_integer([-5, -6, -7, -8, -9]), -5)
         self.assertEqual(max_integer([1, 2]), 2)
-        self.assertEqual(max_integer([7, 8 ,5]), 8)\
+        self.assertEqual(max_integer([7, 8 ,5]), 8)
 
     def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
     
     def test_one_elememt_list(self):
-        self.assertEqual(max_integer9[1], 1)
+        self.assertEqual(max_integer([1]), 1)
 
     def test_one_element_list_negative(self):
         self.assertEqual(max_integer([-1]), -1)
