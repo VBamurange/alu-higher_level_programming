dc92b04dce01a8af9f349087319c3fb2f99dfef3
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-09 09:20:25 -0800

    changes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,55 +1,42 @@
 #!/usr/bin/python3
-"""Create a Square class for the work"""
+"""The square"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Create a Square class for the work"""
+    """class square"""
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Create a Square class for the work
-
-        :param size: The size of the square
-        :param x: The x coordinate of the square
-        :param y: The y coordinate of the square
-        :param id: The id of the square
+    Initialize the new variables
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """
-        Return the size of the square
-
-        :return: The size of the square
+        size function for the square
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-        Set the size of the square
-
-        :param value: The size of the square
+    set size of the square
         """
         self.width = value
         self.height = value
 
     def __str__(self):
         """
-        Return the string representation of the square
-
-        :return: The string representation of the square
+        string representation
         """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     def update(self, *args, **kwargs):
         """
-        Update the square
-
-        :return: The updated square
+        the update function for the square
         """
         if len(args) != 0:
             try:
@@ -81,8 +68,6 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         """
-        Return a dictionary representation of the square
-
-        :return: The dictionary representation of the square
+        dictionary representation of the square
         """
         return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
