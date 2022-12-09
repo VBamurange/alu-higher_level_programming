1dd0a06469ebcd3101c32248ab6aeedbfdda4645
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-09 08:06:52 -0800

    changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -49,3 +49,34 @@ class Rectangle(Base):
                 raise ValueError("height must be > 0")
 
             self.__height = value
+
+        @property
+        def x(self):
+            """function  for x"""
+            return self.__x
+
+        @x.setter
+        def x(self, value):
+            """set for x"""
+            if type(value) !=int:
+                raise TypeError("x must be an integer")
+            if value < 0:
+                raise ValueError("x must be >= 0")
+            self.__x = value
+
+        @property
+        def y(self):
+            """y for function"""
+            return self.__y
+
+        @y.setter
+        def y(self, value):
+            """set for y"""
+
+            if type(value) != int:
+                raise TypeError("y must be an integer")
+
+            if value < 0:
+                raise ValueError("y must be >= 0")
+
+            self.__y = value
