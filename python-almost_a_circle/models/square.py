d415d16b81b63b8c8ab430c77bf6264603f2984b
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-09 09:13:42 -0800

    changes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,37 +1,56 @@
 #!/usr/bin/python3
-"""and now, the square"""
+"""Create a Square class for the work"""
 
 
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """new class square"""
+    """Create a Square class for the work"""
     def __init__(self, size, x=0, y=0, id=None):
-        """initialise variables"""
-        super().__int__(size, size, x, y, id)
+        """
+        Create a Square class for the work
+
+        :param size: The size of the square
+        :param x: The x coordinate of the square
+        :param y: The y coordinate of the square
+        :param id: The id of the square
+        """
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """size function"""
+        """
+        Return the size of the square
 
+        :return: The size of the square
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """set size"""
+        """
+        Set the size of the square
 
+        :param value: The size of the square
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
-        """string representation"""
+        """
+        Return the string representation of the square
 
+        :return: The string representation of the square
+        """
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     def update(self, *args, **kwargs):
-        """update function"""
+        """
+        Update the square
 
+        :return: The updated square
+        """
         if len(args) != 0:
             try:
                 self.id = args[0]
@@ -51,6 +70,7 @@ class Square(Rectangle):
                 self.size = kwargs["size"]
             else:
                 self.size
+
             if "x" in kwargs:
                 self.x = kwargs["x"]
             else:
@@ -59,7 +79,10 @@ class Square(Rectangle):
             if "y" in kwargs:
                 self.y = kwargs["y"]
 
-        def to_dictionary(self):
-            """dictionary function"""
-            
-            return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+    def to_dictionary(self):
+        """
+        Return a dictionary representation of the square
+
+        :return: The dictionary representation of the square
+        """
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
