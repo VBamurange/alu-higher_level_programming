2cc5b9c4b964b7a44cb5e563d89f0477eebf0076
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-09 07:49:35 -0800

    changes

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""first rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """new class Rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width function"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+        @property
+        def height(self):
+            """height function"""
+            return self.__height
+
+        @height.setter
+        def height(self, value):
+            """set height"""
+
+            if type(value) != int:
+                raise TypeError("height must be an integer")
+
+            if value <= 0:
+                raise ValueError("hgeight must be > 0")
+
+            self.__height = value
