d73eca3a4d42a5c1c6baf0a3f9a0203fbd74c77f
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 08:01:27 -0800

    DB changes

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""filter states"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[0], passwd=argv[1], db=argv[2])
+    cursor = db.cursor()
+    cursor.executre("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    my_list = cursor.fetchall()
+    for j in my_list:
+        if j[1][0] == 'N':
+            print(j)
+    cursor.close()
+    db.close()
