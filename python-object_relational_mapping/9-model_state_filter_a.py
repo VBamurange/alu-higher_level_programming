e671cbd5ececc5817f0b10ad9c16c41e7773394b
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 09:53:17 -0800

    DB changes

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -10,12 +10,11 @@ from model_state import Base, State
 if __name__ == "__main__":
     eng = create_engine('mysql+mysqldb://{]:{}@localhost/{}'.format(argv[1],
                                                                     argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
+                                                                    argv[3],
+                                                                    pool_pre_ping= True)
+    Session = sessionmaker(bind=engine)
     session = Session()
-    s = '%a%'
-    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-    session.close()
+
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
