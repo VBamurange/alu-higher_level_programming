60af28439569473329b17ed905a1fd32ebea0ddb
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 08:23:11 -0800

    DB changes

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,9 +5,8 @@
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], 
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY \
