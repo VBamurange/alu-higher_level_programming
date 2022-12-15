a4764e2696d25f36edcbf162c09d96184b5a3d05
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 10:03:37 -0800

    DB changes

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""contains a"""
+"""Contains letter """
 
-import sys
+
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -9,12 +9,14 @@ from sys import argv
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{]:{}@localhost/{}'
-                           .format(sys.argv[1],sys.argv[2],sys.argv[3]),
-                            pool_pre_ping= True)
-    Session = sessionmaker(bind=engine)
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]))
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
-
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    s = '%a%'
+    states = session.query(State).filter(State.name.like(s)).order_by(State.id)
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
