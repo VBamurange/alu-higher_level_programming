9aa5f14bb2e70ea75ed50643e6adb9c0fea17c0f
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 10:33:43 -0800

    DB changes

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""cities in state"""
+
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base, State
+
+
+class City(Base):
+    """Representation of a city"""
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
