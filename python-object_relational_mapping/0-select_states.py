7b46e3bbeb3fa77ea3bd6b164c08e68d523a625c
Vicky Bamurange <v.bamurange@alustudent.com>
2022-12-15 07:51:48 -0800

    DB changes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                     passwd=argv[2], db=argv[3], charset="utf8")
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY  id ASC")
-my_list = cursor.fetchall()
-for j in my_list:
-    print(j)
-cursor.close()
-db.close()
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY  id ASC")
+    my_list = cursor.fetchall()
+    for j in my_list:
+        print(j)
+    cursor.close()
+    db.close()
