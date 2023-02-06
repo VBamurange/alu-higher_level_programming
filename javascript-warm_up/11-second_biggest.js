456397b810efd854ec7bb1064d7d53a146926ec7
Vicky Bamurange <v.bamurange@alustudent.com>
2023-02-06 11:38:53 -0800

    java changes

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const x = process.argv.length;
+const y = [];
+
+switch (x) {
+  case 2:
+  case 3:
+    console.log(0);
+    break;
+	
+  default:
+    for (let i = 2; i < x; i++) {
+      y.push(process.argv[i]);
+    }
+    y.sort((a, b) => b - a);
+    console.log(y[1]);
+    break;
+} 
