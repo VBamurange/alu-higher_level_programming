5952eb9e554cb1c8144acdca6a525a18eb4d96df
Vicky Bamurange <v.bamurange@alustudent.com>
2023-02-06 13:01:10 -0800

    java objects

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -2,15 +2,15 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-   if ( w > 0 && h > 0) {
-     this.width = w;
-     this.height = h;
+    if ( w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
    }
  }
 
- print () {
-   for (let i = 0; i < this.height; i++) {
-    console.log('X'.repeat(this.width));
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
    }
  }
 };	   
