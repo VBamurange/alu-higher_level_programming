62383f528afe19c1f664a816a11936628eeee7b5
Vicky Bamurange <v.bamurange@alustudent.com>
2023-02-07 08:25:09 -0800

    java objects

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,32 +1,32 @@
 #!/usr/bin/node
 const Rectangle = class Rectangle {
-   constructor (w, h) { 
-     if (w > 0 && h > 0) {
-       this.width = w;
-       this.height = h;
-     }
-   }
+  constructor (w, h) { 
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-   print () {
-     let prints = '';
-     for (let x = 0; x < this.height; x++) {
-       for (let y = 0; y < this.width; y++) {
-	 prints = prints + 'X';
-       }
-       console.log(prints);
-       prints = '';
+  print () {
+    let prints = '';
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        prints = prints + 'X';
       }
-   }
+      console.log(prints);
+      prints = '';
+    }
+  }
 
-   rotate () {
-     const p = this.width;
-     this.width = this.height;
-     this.height = p;
-   }
+  rotate () {
+    const p = this.width;
+    this.width = this.height;
+    this.height = p;
+  }
 
-   double () {
-     this.width *= 2;
-     this.height *= 2
-   }
+  double () {
+    this.width *= 2;
+    this.height *= 2
+  }
 };
 module.exports = Rectangle;
